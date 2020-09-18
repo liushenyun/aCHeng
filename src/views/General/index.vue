f0681076b0ad9beb7ae1007beabc3e24455abc84
liushenyun <960416437@qq.com>
2020-09-18 23:03:05 +0800

    a

--- src/views/General/index.vue
+++ src/views/General/index.vue
@@ -2,15 +2,65 @@
   <div class="General-outer">
     <div class="g-top">
       <div class="get-same gt-left">
-        <p>gg</p>
+        <p class="gs-title">Get Started by Exploring</p>
+        <div class="router-wrap">
+          <div class="router-same">
+            <div class="btn">
+              <img src="../../image/ic_selected_Metagenome.png" alt="">
+              <span>Metagenome</span>
+            </div>
+            <p>See project and its metadata</p>
+          </div>
+          <div class="router-same">
+            <div class="btn">
+              <img src="../../image/ic_selected_Project.png" alt="">
+              <span>Project</span>
+            </div>
+            <p>Search project to show it's metadata summary</p>
+          </div>
+        </div>
+        <div class="router-wrap">
+          <div class="router-same">
+            <div class="btn">
+              <img src="../../image/ic_selected_MetaData.png" alt="">
+              <span>Metadata</span>
+            </div>
+            <p>Search and download metada</p>
+          </div>
+          <div class="router-same">
+            <div class="btn">
+              <img src="../../image/ic_selected_Search.png" alt="">
+              <span>Search</span>
+            </div>
+            <p>Adwanced search for project</p>
+          </div>
+        </div>
       </div>
       <div class="get-same gt-right">
-        <p>gg</p>
-        <div id="main" style="width: 600px;height:400px;"></div>
+        <p class="gs-title">Sample by primary metadata </p>
+        <div id="main" style="width: 100%;height: 300px;"></div>
       </div>
     </div>
     <div class="g-bottom get-same">
-      <p>gg</p>
+      <p class="gs-title">Data Summary</p>
+      <ul>
+        <li>
+          <div><img src="../../image/ic_Projects.png" alt=""><p>Projects</p></div>
+          <span>44</span>
+        </li>
+        <li>
+          <div><img src="../../image/ic_Metada.png" alt=""><p>Projects</p></div>
+          <span>44</span>
+        </li>
+        <li>
+          <div><img src="../../image/ic_Samples.png" alt=""><p>Projects</p></div>
+          <span>44</span>
+        </li>
+        <li>
+          <div><img src="../../image/ic_Files.png" alt=""><p>Projects</p></div>
+          <span>44</span>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
